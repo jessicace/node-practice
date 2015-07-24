7747cf95e52662734c1f40ce2e8f76b426784305
Jessica Claire Edwards <jessica@eml.cc>
2015-07-24 17:09:26 +1000

    Completed http collect exercise

---
+++ http_collect.js
@@ -0,0 +1,38 @@
+// Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument.
+// Collect all data from the server, then write two lines to the console.
+// The first line you write should be an integer representing the number of characters received from the server.
+// The second line should contain the complete String of characters sent by the server.
+
+var http = require('http');
+
+// My solution
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8');
+  var totalCharacters = 0;
+  var completeString = "";
+  response.on("data", function(data) {
+    totalCharacters += data.length;
+    completeString += data;
+  });
+  response.on("end", function() {
+    console.log(totalCharacters);
+    console.log(completeString);
+  });
+});
+
+
+// Alternative using bl
+// var http = require('http')
+// var bl = require('bl')
+//    
+// http.get(process.argv[2], function (response) {
+//  response.pipe(bl(function (err, data) {
+//                  if (err) {
+//                    return console.error(err);
+//                  }
+//                  data = data.toString();
+//                  console.log(data.length);
+//                  console.log(data);
+//                }));  
+// });
+//
