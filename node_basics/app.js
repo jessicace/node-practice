6f7fdc1140c0552d9bec8f9a3e23766e13005021
Jessica Claire Edwards <jessica@eml.cc>
2015-07-27 17:43:16 +1000

    Separated data into separate profile module

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,30 +1,3 @@
-var http = require('http');
-var username = 'jessicace';
+var profile = require('./profile.js');
 
-// Print the data.
-function printMessage(username, badgeCount, points) {
- console.log(username + " has " + badgeCount + " total badges and " + points + " points in JavaScript.");
-}
-
-// Print out error messages.
-function printError(error) {
-  console.error(error.message);
-}
-
-
-// Connect to the API URL.
-var request =
-  http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
-    var body = "";
-    // Read the data from the response.
-    response.on('data', function (chunk) {
-      body += chunk;
-    });
-    response.on('end', function() {
-      var profile = JSON.parse(body);
-      printMessage(username, profile.badges.length, profile.points.JavaScript);
-    });
-    // Parse the data.
-  });
-
-request.on("error", printError(error));
+profile.get('jessicace');
---
+++ node_basics/profile.js
@@ -0,0 +1,46 @@
+var http = require('http');
+
+
+// Print the data.
+function printMessage(username, badgeCount, points) {
+ console.log(username + " has " + badgeCount + " total badges and " + points + " points in JavaScript.");
+}
+
+// Print out error messages.
+function printError(error) {
+  console.error(error.message);
+}
+
+function get(username) {
+// Connect to the API URL.
+var request =
+  http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
+    var body = "";
+    // Read the data from the response.
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+    response.on('end', function() {
+      if (response.statusCode === 200) {
+        try {
+          // Parse the data
+          var profile = JSON.parse(body);
+          // Print the data
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+        } catch(error) {
+          // Parse Error
+          printError(error);
+        }
+      } else {
+        // Status Code Error
+        printError({
+          message: "There was an error getting the profile for " + username + ". (" +
+            http.STATUS_CODES[response.statusCode] + ")"
+        });
+      }
+    });
+  });
+request.on("error", printError(error));
+}
+
+module.exports.get = get;
