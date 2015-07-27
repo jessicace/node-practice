72146ed8a8fcee0a046c57fb5a36d4b36e2ca8a9
Jessica Claire Edwards <jessica@eml.cc>
2015-07-27 18:50:25 +1000

    First node web application

---
+++ dynamic_node_site/app.js
@@ -0,0 +1,23 @@
+// Look at a user's badge count and JavaScript points from a web browser.
+// Perform profile look ups and serve our template via HTTP.
+
+// 1. Create a web server
+var http = require('http');
+http.createServer(function (request, response) {
+  homeRoute(request, response);
+}).listen(3000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:3000');
+
+// 2. Handle HTTP route GET / and POST / e.g. Home
+function homeRoute(request, response) {
+  // show search
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  setInterval(function() {
+    response.write(new Date() + '\n');
+  }, 1000);
+  // response.end('Goodbye World!\n');
+}
+// 3. Handle HTTP route GET /:username e.g. jessicace
+
+// 4. Function that handles the reading of files and merge in value
