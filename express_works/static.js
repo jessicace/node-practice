42b1d0847a698a0d399e9b11bd27d24206dd656a
Jessica Claire Edwards <jessica@eml.cc>
2015-07-28 14:58:39 +1000

    Solved, installed express

---
+++ express_works/static.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var portNumber = process.argv[2];
+var pathName = process.argv[3];
+
+app.use(express.static(pathName || path.join(__dirname, 'public')));
+
+app.listen(portNumber);
